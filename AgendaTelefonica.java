25ebd8922eb38ca5ff3f4f575af2bcf6fd49224d
Víctor <victorlopez94leon@gmail.com>
2016-03-04 12:04:03 +0100

    Creadas las clases AgendaTelefonica y TestAgendaTelefonica

---
+++ AgendaTelefonica.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class AgendaTelefonica here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AgendaTelefonica
+{
+
+    /**
+     * Constructor de un objeto de la clase AgendaTelefonica
+     */
+    public AgendaTelefonica()
+    {
+        
+    }
+}
