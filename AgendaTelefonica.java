2f37879320674d6f5cec332ad5654834ccceffaf
Víctor <victorlopez94leon@gmail.com>
2016-03-04 12:29:25 +0100

    Añadidas las cabeceras de los métodos de la clase AgendaTelefonica e implementado el código de la clase TestAgendaTelefonica

--- AgendaTelefonica.java
+++ AgendaTelefonica.java
@@ -1,4 +1,4 @@
-
+import java.util.HashMap;
 /**
  * Write a description of class AgendaTelefonica here.
  * 
@@ -7,12 +7,31 @@
  */
 public class AgendaTelefonica
 {
-
+    
     /**
-     * Constructor de un objeto de la clase AgendaTelefonica
+     * Constructor de un objeto de la clase AgendaTelefo
      */
     public AgendaTelefonica()
     {
-        
+
+    }
+
+    /**
+     * Permite almacenar un nuevo número en la agenda indicando el nombre del contacto
+     * y su numero de telefono asociado
+     */
+    public void enterNumber(String name, String number) 
+    {
+
+    }
+
+    /**
+     * Devuelve el numero de teléfono del contacto indicado. Si el contacto no existe, 
+     * devuelve null.
+     */
+    public String lookupNumber(String name)
+    {
+
     }
 }
+
--- TestAgendaTelefonica.java
+++ TestAgendaTelefonica.java
@@ -7,6 +7,15 @@
  */
 public class TestAgendaTelefonica
 {
-
-    
+    public void test()
+    {
+        AgendaTelefonica agenda = new AgendaTelefonica();
+        //Se hacen correctamente consultas a una agenda vacía.
+        agenda.lookupNumber("nombre");
+        //Se introducen correctamente contactos.
+        agenda.enterNumber("Pepe","987654321");
+        //Se consulta correctamente el teléfono de un contacto existente.
+        agenda.lookupNumber("Pepe");
+        //Se consulta correctamente el teléfono de un contacto inexistente.
+    }
 }
